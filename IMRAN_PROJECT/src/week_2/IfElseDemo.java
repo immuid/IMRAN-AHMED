9e7bbe2a43a940495c63d583bd94ba5622426549
Imran Ahmed M (21222) <84715478+immuid@users.noreply.github.com>
2022-01-24 23:22:18 +0530

    1STCOMMIT

---
+++ IMRAN_PROJECT/src/week_2/IfElseDemo.java
@@ -0,0 +1,51 @@
+package week_2;
+
+public class IfElseDemo {
+	public static void main(String[] args) {
+		int i=10;
+		if(i>20) {
+			System.out.println("I is greater than 20...");
+		}
+		else if(i==10) {
+			System.out.println("I is equal to 10...");
+		}
+		else {
+			System.out.println("I is lesser than 20...");
+		}
+		IfElseDemo obj=new IfElseDemo();
+		String result=obj.met(i);
+		System.out.println("the result is "+result);
+		
+		boolean b= true;
+		obj.met2(b);
+		
+		String str ="hello";
+		obj.met3(str);
+	}
+	String met(int i) {
+		if(i==10) {
+			return "'i' is equal to 10";
+		}
+		else {
+			return "condition not satisfied";
+		}
+	}
+	void met2(boolean valid) {
+		if(valid) {
+			System.out.println( "The Valid value is true");
+		}
+		else {
+			System.out.println( "The Valid value is False");
+		}			
+	}
+	void met3(String s) {
+		if(s.equals("Hello")) {
+			System.out.println("The String is equal");
+		}
+		else {
+			System.out.println("The String is not Equal");
+		}
+	}
+	}
+	
+
